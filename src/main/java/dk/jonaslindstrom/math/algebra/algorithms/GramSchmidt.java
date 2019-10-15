258d27ecccdade4451c4c136fa298e576f0e2b85
Jonas Lindstrøm <jonas.l.jensen@alexandra.dk>
2019-10-15 13:09:37 +0200

    Simplify Gram-Schmidt

--- src/main/java/dk/jonaslindstrom/math/algebra/algorithms/GramSchmidt.java
+++ src/main/java/dk/jonaslindstrom/math/algebra/algorithms/GramSchmidt.java
@@ -3,36 +3,33 @@ package dk.jonaslindstrom.math.algebra.algorithms;
 import dk.jonaslindstrom.math.algebra.abstractions.HilbertSpace;
 import dk.jonaslindstrom.math.algebra.abstractions.VectorSpace;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
-public class GramSchmidt<Vector, Scalar> implements Function<List<Vector>, List<Vector>> {
+public class GramSchmidt<V, S> implements Function<List<V>, List<V>> {
 
-  private VectorSpace<Vector, Scalar> vectorSpace;
-  private BiFunction<Vector, Vector, Scalar> innerProduct;
+  private VectorSpace<V, S> vectorSpace;
+  private BiFunction<V, V, S> innerProduct;
 
-  public GramSchmidt(HilbertSpace<Vector, Scalar> S) {
+  public GramSchmidt(HilbertSpace<V, S> S) {
     this(S, S::innerProduct);
   }
 
-  public GramSchmidt(VectorSpace<Vector, Scalar> V,
-      BiFunction<Vector, Vector, Scalar> innerProduct) {
+  public GramSchmidt(VectorSpace<V, S> V,
+      BiFunction<V, V, S> innerProduct) {
     this.vectorSpace = V;
     this.innerProduct = innerProduct;
   }
 
   @Override
-  public List<Vector> apply(List<Vector> vectors) {
-    List<Vector> U = new ArrayList<>();
-    LinkedList<Vector> V = new LinkedList<>(vectors);
+  public List<V> apply(List<V> vectors) {
+    List<V> U = new ArrayList<>();
 
-    Projection<Vector, Scalar> proj = new Projection<>(vectorSpace, innerProduct);
-    while (!V.isEmpty()) {
-      Vector v = V.poll();
-      for (Vector u : U) {
-        Vector p = proj.apply(v, u);
+    Projection<V, S> proj = new Projection<>(vectorSpace, innerProduct);
+    for (V v : vectors) {
+      for (V u : U) {
+        V p = proj.apply(v, u);
         v = vectorSpace.subtract(v, p);
       }
       U.add(v);
