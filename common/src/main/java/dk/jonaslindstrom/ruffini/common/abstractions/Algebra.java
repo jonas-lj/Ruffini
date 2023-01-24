a0d7375d2052f179f038a6a498a812d3f3a31836
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-01-24 13:44:48 +0100

    Fix javadoc

--- common/src/main/java/dk/jonaslindstrom/ruffini/common/abstractions/Algebra.java
+++ common/src/main/java/dk/jonaslindstrom/ruffini/common/abstractions/Algebra.java
@@ -3,7 +3,7 @@ package dk.jonaslindstrom.ruffini.common.abstractions;
 public interface Algebra<V, S, R extends Ring<S>> extends Module<V, S, R> {
 
     /**
-     * Return the result of the multiplication of <i>u</i> and <i>v</i></i> in this algebra.
+     * Return the result of the multiplication of <i>u</i> and <i>v</i> in this algebra.
      */
     V multiply(V v, V u);
 
--- common/src/main/java/dk/jonaslindstrom/ruffini/common/abstractions/EuclideanDomain.java
+++ common/src/main/java/dk/jonaslindstrom/ruffini/common/abstractions/EuclideanDomain.java
@@ -7,7 +7,7 @@ import java.math.BigInteger;
 public interface EuclideanDomain<E> extends Ring<E> {
 
     /**
-     * Returns a pair <i>(q, r)</i> such that <i>a = qb + r</i> and <i>0 ≤ f(r) < f(b)</i> where
+     * Returns a pair <i>(q, r)</i> such that <i>a = qb + r</i> and <i>0 &le; f(r) &lt; f(b)</i> where
      * <i>f</i> is the Euclidean function (see {@link #norm(E)} for this domain.
      */
     Pair<E, E> divisionWithRemainder(E a, E b);
