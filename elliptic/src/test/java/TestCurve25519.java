5c03a05b2815df8a0a5937b2574ffac2d9abb208
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-02-18 11:18:19 +0100

    Fix test

--- elliptic/src/test/java/TestCurve25519.java
+++ elliptic/src/test/java/TestCurve25519.java
@@ -15,12 +15,12 @@ public class TestCurve25519 {
         Curve25519 curve25519 = new Curve25519();
         BigInteger n = curve25519.decodeScalar(Hex.decodeHex("a8abababababababababababababababababababababababababababababab6b"));
         AffinePoint<BigInteger> x = curve25519.scale(n, Curve25519.BASE_POINT);
-        AffinePoint<BigInteger> expected = curve25519.decodePoint(Hex.decodeHex("e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859"));
+        AffinePoint<BigInteger> expected = curve25519.decodePoint(Hex.decodeHex("02e3712d851a0e5d79b831c5e34ab22b41a198171de209b8b8faca23a11c624859"));
         Assert.assertEquals(expected.x(), x.x());
 
         n = curve25519.decodeScalar(Hex.decodeHex("c8cdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcdcd4d"));
         x = curve25519.scale(n, Curve25519.BASE_POINT);
-        expected = curve25519.decodePoint(Hex.decodeHex("b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f"));
+        expected = curve25519.decodePoint(Hex.decodeHex("02b5bea823d9c9ff576091c54b7c596c0ae296884f0e150290e88455d7fba6126f"));
         Assert.assertEquals(expected.x(), x.x());
     }
 
