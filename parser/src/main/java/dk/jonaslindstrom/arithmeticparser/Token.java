b9cd49aae7bad5d7678184f9c855c594f33e61a2
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-02-19 20:17:15 +0100

    Add parser

---
+++ parser/src/main/java/dk/jonaslindstrom/arithmeticparser/Token.java
@@ -0,0 +1,31 @@
+package dk.jonaslindstrom.arithmeticparser;
+
+/**
+ * Instances of this class represents a token in an arithmetic expression.
+ */
+public class Token {
+
+    private final String representation;
+    private final Type type;
+    public Token(String representation, Type type) {
+        this.representation = representation;
+        this.type = type;
+    }
+
+    public String getRepresentation() {
+        return representation;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String toString() {
+        return representation;
+    }
+
+    public enum Type {
+        FUNCTION, NUMBER, OPERATOR, VARIABLE, LEFT_PARANTHESIS;
+    }
+
+}
