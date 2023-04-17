6846ca4df79ce73aa0212f0513b47c8e041965da
Jonas Lindstrøm <jonas-lj@users.noreply.github.com>
2023-04-17 23:31:45 +0200

    Remove unused import

--- demos/src/main/java/demo/BLS12381.java
+++ demos/src/main/java/demo/BLS12381.java
@@ -2,7 +2,6 @@ package demo;
 
 import dk.jonaslindstrom.ruffini.common.algorithms.Power;
 import dk.jonaslindstrom.ruffini.common.exceptions.InvalidParametersException;
-import org.apache.commons.codec.DecoderException;
 
 import java.security.NoSuchAlgorithmException;
 
@@ -10,7 +9,7 @@ import static dk.jonaslindstrom.ruffini.elliptic.structures.bls12381.BLS12381.*;
 
 public class BLS12381 {
 
-    public static void main(String[] arguments) throws DecoderException, InvalidParametersException, NoSuchAlgorithmException {
+    public static void main(String[] arguments) throws InvalidParametersException, NoSuchAlgorithmException {
 
         // Check that e(5g, h) = e(g, 5h) = e(g,h)^5 as expected
         System.out.println(PAIRING.apply(G1.scale(5, G1_GENERATOR), G2_GENERATOR));
